bd189119619b104ba4a25b776d36d393e179b422
Saadman Farhad <saadmanheavy@gmail.com>
2023-04-16 22:39:28 +0200

    Cleanup

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import HomeItem from './HomeItem';
-import { fetchMenu, addItem } from '../../actions';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from "react";
+import HomeItem from "./HomeItem";
+import { fetchMenu, addItem } from "../../actions";
+import { useSelector, useDispatch } from "react-redux";
 
 const Home = () => {
-  // const [item, setItem] = useState(undefined);
   const dispatch = useDispatch();
-  const menus = useSelector(state => state.menu);
+  const menus = useSelector((state) => state.menu);
 
-  const addItemToCart = item => {
+  const addItemToCart = (item) => {
     if (item.quantity) {
       dispatch(addItem(item));
     }
@@ -16,51 +15,6 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(fetchMenu());
-    // const items = [
-    //   {
-    //     name: 'Pizza',
-    //     price: '20',
-    //     type: 'Italian',
-    //     img:
-    //       'https://i.picsum.photos/id/969/400/400.jpg?hmac=DDkXJzyjx2-VfaiggGR_3wFXvtI3teTQVb8nv_eleZ8',
-    //     description: 'Yummy Pizza!!!'
-    //   },
-    //   {
-    //     name: 'Pizza',
-    //     price: '20',
-    //     type: 'Italian',
-    //     img:
-    //       'https://i.picsum.photos/id/969/400/400.jpg?hmac=DDkXJzyjx2-VfaiggGR_3wFXvtI3teTQVb8nv_eleZ8',
-    //     description: 'Yummy Pizza!!!'
-    //   },
-    //   {
-    //     name: 'Pizza',
-    //     price: '20',
-    //     type: 'Italian',
-    //     img:
-    //       'https://i.picsum.photos/id/969/400/400.jpg?hmac=DDkXJzyjx2-VfaiggGR_3wFXvtI3teTQVb8nv_eleZ8',
-    //     description: 'Yummy Pizza!!!'
-    //   },
-    //   {
-    //     name: 'Pizza',
-    //     price: '20',
-    //     type: 'Italian',
-    //     img:
-    //       'https://i.picsum.photos/id/969/400/400.jpg?hmac=DDkXJzyjx2-VfaiggGR_3wFXvtI3teTQVb8nv_eleZ8',
-    //     description:
-    //       'Yummy Pizzaasdasd asda sdasd asdaaaaaaaaaaaaaaaaasd asd asdas dasdqwoind iansd ansdpanw pasndpi anw fpianscpi ansfp idnaspiic naspinf paisnc piasncpaisnd pian fpians pia n fpinap ifnapscnapnapnf apins pian fpin!!!'
-    //   },
-    //   {
-    //     name: 'Pizza',
-    //     price: '20',
-    //     type: 'Italian',
-    //     img:
-    //       'https://i.picsum.photos/id/969/400/400.jpg?hmac=DDkXJzyjx2-VfaiggGR_3wFXvtI3teTQVb8nv_eleZ8',
-    //     description:
-    //       'Yummy Pizzaasdasd asda sdasd asdaaaaaaaaaaaaaaaaasd asd asdas dasdqwoind iansd ansdpanw pasndpi anw fpianscpi ansfp idnaspiic naspinf paisnc piasncpaisnd pian fpians pia n fpinap ifnapscnapnapnf apins pian fpin!!!'
-    //   }
-    // ];
-    // setItem(items);
   }, [dispatch]);
 
   return menus ? (
@@ -69,7 +23,7 @@ const Home = () => {
         <h1 className="f3 f2-m f1-l fw2 black-70 mv3">Menu</h1>
       </header>
       <div className="cf">
-        {menus.map(menu => (
+        {menus.map((menu) => (
           <div key={menu._id} className="fl w-100 w-50-ns w-50-m w-50-l">
             <HomeItem menu={menu} addItem={addItemToCart} />
           </div>
